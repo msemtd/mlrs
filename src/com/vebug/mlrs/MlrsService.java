94efde945331db7014a1ad3e9d537000214c0932
Michael Erskine <msemtd@googlemail.com>
2015-06-20 11:48:31 +0100

    starting eclipse project

---
+++ src/com/vebug/mlrs/MlrsService.java
@@ -0,0 +1,18 @@
+package com.vebug.mlrs;
+
+public class MlrsService {
+
+	public static void main(String[] args) {
+		// service launch parameters
+		// serial port for DUE
+		// serial parameters
+		// escape mechanism
+		// timeouts
+		// TODO read XML config
+		// TODO sockets control interface
+		// TODO log4j
+		
+
+	}
+
+}
